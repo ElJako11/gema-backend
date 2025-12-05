1ce2fdbb766423d758b40be3b9a5985c0c946a60
HyeG18 <sebastigp@gmail.com>
2025-12-04 21:05:05 -0400

    fix: tecnico service put y delete agregado

--- src/services/tecnico/tecnico.service.ts
+++ src/services/tecnico/tecnico.service.ts
@@ -1,51 +1,119 @@
 import { db } from '../../config/db';
-import { usuarios } from '../../tables/usuarios';
-import { CreateTecnicoParams } from '../../types/types';
+// import { usuarios } from '../../tables/usuarios';
+// import { CreateTecnicoParams } from '../../types/types';
 import { eq } from 'drizzle-orm';
+import { CreateTecnicoParams } from '../../types/tecnico';
+import { tecnico } from '../../tables/tecnico';
 
-export const createTecnico = async ({
-  Nombre,
-  Correo,
-}: CreateTecnicoParams) => {
+//Get all tecnicos
+export const getAllTecnicos = async () => {
   try {
-    // Validate input
-    if (!Nombre || !Correo) {
-      throw new Error('Nombre y Correo son campos obligatorios');
-    }
-
-    // Insert into usuarios table
-    const insertedUser = await db
-      .insert(usuarios)
-      .values({
-        Nombre,
-        Correo,
-        Tipo: 'SUPERVISOR',
-      })
-      .returning({ Id: usuarios.Id });
-
-    if (insertedUser.length === 0) {
-      throw new Error('Error al crear el usuario');
-    }
-
-    return {
-      message: 'Usuario creado correctamente',
-      userId: insertedUser[0].Id,
-    };
+    const tecnicosList = await db
+      .select()
+      .from(tecnico);
+    return tecnicosList;
   } catch (error) {
-    console.error('Error creating tecnico:', error);
-    throw new Error('Error al crear el tecnico');
+    console.error('Error al obtener tecnicos:', error);
+    throw new Error('Error al obtener los tecnicos');
   }
 };
 
-export const getAllTecnicos = async () => {
-  const tecnicos = await db
-    .select({
-      Id: usuarios.Id,
-      Nombre: usuarios.Nombre,
-      Correo: usuarios.Correo,
-    })
-    .from(usuarios)
-    .where(eq(usuarios.Tipo, 'SUPERVISOR'));
+//Create tecnico
+export const createTecnico = async (params: CreateTecnicoParams) => {
+  // Validate input
+    if (!params.nombre || !params.direccion) {
+      throw new Error('Nombre y Direccion son campos obligatorios');
+    }
+  try {
+    const insertedTecnico = await db
+      .insert(tecnico)
+      .values(params)
+      .returning();
+    return insertedTecnico[0];
+  } catch (error) {
+    console.error('Error al crear tecnico:', error);
+    throw new Error('Error al crear el tecnico');
+  }
+}
 
-  return tecnicos;
+//Put tecnico
+export const updateTecnico = async (id : number, params : CreateTecnicoParams) => {
+  if (isNaN(id)) {
+    throw new Error('ID inválido');
+  }
+  try {
+    const updatedTecnico = await db
+      .update(tecnico)
+      .set(params)
+      .where(eq(tecnico.idTecnico, id))
+      .returning();
+    return updatedTecnico[0];
+  } catch (error) {
+    console.error('Error al actualizar tecnico:', error);
+    throw new Error('Error al actualizar el tecnico');
+  }
+}
+
+//Delete tecnico
+export const deleteTecnico = async (id : number) => {
+  if (isNaN(id)) throw new Error('ID inválido');
+  try {
+    const deleted = await db
+      .delete(tecnico)
+      .where(eq(tecnico.idTecnico, id))
+      .returning();
+
+    return deleted[0];
+
+  } catch (error) {
+    console.error('Error al eliminar tecnico:', error);
+    throw new Error('Error al eliminar el tecnico');
+  }
 };
+
+// export const createTecnico = async ({
+//   Nombre,
+//   Correo,
+// }: CreateTecnicoParams) => {
+//   try {
+//     // Validate input
+//     if (!Nombre || !Correo) {
+//       throw new Error('Nombre y Correo son campos obligatorios');
+//     }
+
+//     // Insert into usuarios table
+//     const insertedUser = await db
+//       .insert(usuarios)
+//       .values({
+//         Nombre,
+//         Correo,
+//         Tipo: 'SUPERVISOR',
+//       })
+//       .returning({ Id: usuarios.Id });
+
+//     if (insertedUser.length === 0) {
+//       throw new Error('Error al crear el usuario');
+//     }
+
+//     return {
+//       message: 'Usuario creado correctamente',
+//       userId: insertedUser[0].Id,
+//     };
+//   } catch (error) {
+//     console.error('Error creating tecnico:', error);
+//     throw new Error('Error al crear el tecnico');
+//   }
+// };
+
+// export const getAllTecnicos = async () => {
+//   const tecnicos = await db
+//     .select({
+//       Id: usuarios.Id,
+//       Nombre: usuarios.Nombre,
+//       Correo: usuarios.Correo,
+//     })
+//     .from(usuarios)
+//     .where(eq(usuarios.Tipo, 'SUPERVISOR'));
+
+//   return tecnicos;
+// };
