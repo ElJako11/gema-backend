e16d7ed9472a9765854b6c34392727a052b868e4
ElJako11 <joaquimejuhah@gmail.com>
2025-12-15 14:55:57 -0400

    feat(calendario): Controller logic implemented

--- src/controllers/calendario/calendario.controller.ts
+++ src/controllers/calendario/calendario.controller.ts
@@ -1,12 +1,35 @@
 import { Request, Response } from 'express';
+import {
+  getAllEventosMensuales,
+  getAllEventosSemanales,
+} from '../../services/calendario/calendario.service';
 
-export const getAllEventosByFechaHandler = (req: Request, res: Response) => {
-  const { date } = req.query;
+export const getAllEventosByFechaHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { date, filter } = req.query;
 
   if (!date) {
     res
       .status(400)
       .json({ error: 'Se requiere la fecha para realizar la busqueda' });
+    return;
+  }
+
+  let result = {};
+
+  try {
+    if (filter === 'mensual') {
+      result = await getAllEventosMensuales(date as string);
+    } else {
+      result = await getAllEventosSemanales(date as string);
+    }
+
+    res.status(200).json(result);
+    return;
+  } catch (error) {
+    res.status(500).json({ error: 'Error interno del servidor' });
     return;
   }
 };
