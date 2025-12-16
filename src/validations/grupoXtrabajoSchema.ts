253c398fa9b8d7cdb088cc1bc3b18d0ebd7bc4bb
HyeG18 <sebastigp@gmail.com>
2025-12-15 21:36:10 -0400

    fix: corregido validation de grupoXtrabajo

--- src/validations/grupoXtrabajoSchema.ts
+++ src/validations/grupoXtrabajoSchema.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { positiveIntIdCoercion } from './globalTypeSchema'; // Asumo que tienes esto
+import { positiveIntId, positiveIntIdCoercion } from './globalTypeSchema'; // Asumo que tienes esto
 
 // 1. Para validar cuando solo viene un ID en la URL (ej: /trabajos/:id/grupos)
 // IMPORTANTE: La clave debe llamarse 'id' porque en la ruta usas :id
@@ -10,6 +10,7 @@ export const urlIdParamSchema = z.object({
 // 2. Para el Body del POST (Solo enviamos el ID del grupo, el del trabajo ya está en la URL)
 export const assignGrupoBodySchema = z.object({
     idG: positiveIntIdCoercion,
+    idT: positiveIntIdCoercion,
 });
 
 // 3. Para el DELETE (Aquí recibimos DOS IDs en la URL)
